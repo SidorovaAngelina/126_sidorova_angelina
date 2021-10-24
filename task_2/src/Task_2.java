20686ae21c50fb6558267593ed9c7ea902fa20cb
SidorovaAngelina <lina_sidorova03@mail.ru>
2021-10-24 18:41:24 +0300

    task 2

---
+++ task_2/src/Task_2.java
@@ -0,0 +1,62 @@
+public class Task_2 implements Task_2_base {
+    @Override
+    public int subtask_1_while(int num) {
+        // Найти максимальное число, являющееся полным квадратом,
+        // не превосходящее заданное натуральное num
+        int max=0;
+        for(int i=0;i*i<=num;++i){
+            max=i*i;
+        }
+        return max;
+    }
+
+    @Override
+    public int subtask_2_while(int num) {
+        // Последовательность целых чисел p(n) определяется следующим образом:
+        // p(0) = 1
+        // p(k) = 2 * p(k - 1) + 6, k > 0
+        //Найти элемент последовательности с номером num
+        int p=1;
+        int k=1;
+        if(num==0){
+            return 1;
+        }
+        do{
+            p=2*p+6;
+            k++;
+        }while (k<=num);
+        return p;
+    }
+
+    @Override
+    public boolean subtask_3_while(int num, int base) {
+        // Проверить, является ли число num натуральной степенью числа base
+        int b=base;
+        while(b<num){
+            b=b*base;
+        }
+        if (b==num)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public int subtask_4_while(int start, int end) {
+        // Вычислить, за какое минимальное число операций
+        // вычесть 1
+        // поделить на 2
+        // число start можно превратить в end. Гарантируется, что start >= end >= 1
+        int count=0;
+        while(start>end){
+            if((start%2==0)&&(start/2>=end)){
+                start=start/2;
+            }
+            else{
+                start--;
+            }
+            count++;
+        }
+        return count;
+    }
+}
