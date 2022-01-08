7cb08acd7460892150bc5b0edf44c37311ba1dee
SidorovaAngelina <lina_sidorova03@mail.ru>
2022-01-09 01:32:41 +0300

    task 10
    test create_CreateEmptyContracts_ContractCountEqualsZero has been created

--- task_10/src/Tests/ContractsTest.java
+++ task_10/src/Tests/ContractsTest.java
@@ -5,5 +5,10 @@ import org.junit.*;
 
 
 public class ContractsTest extends Assert{
-
+    @Test
+    public void create_CreateEmptyContracts_ContractCountEqualsZero(){
+        Contract contract = Contract.create();
+        assertEquals(0, contract.getContractsCount());
+    }
 }
+
