dd07615e62f5dc7ce65518cc55c4a5af73ef08ab
SidorovaAngelina <lina_sidorova03@mail.ru>
2022-01-10 23:22:16 +0300

    task 10
    starting again

--- task_10/src/Core/Contracts.java
+++ task_10/src/Core/Contracts.java
@@ -2,47 +2,7 @@ package Core;
 
 import javax.swing.text.Document;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Contracts {
-    private int ContractsCount;
-    private HashMap<String, Document> data;
-
-    public Contracts(){
-        data = new HashMap<>();
-
-    }
-
-    private Contracts(){
-        ContractsCount = 0;
-    }
-    public void addContract(String number, String date){
-        StringBuilder error = new StringBuilder();
-        if(number==null)
-            error.append("Number can't be null\n");
-        if(date==null)
-            error.append("Date can't be null\n");
-        if(!error.isEmpty())
-            throw new IllegalArgumentException(error.toString());
-        contractsCount++;
-    }
-
-    public void addPayDoc(int sum, int orderNumber, PayDocType paymentType, String contractNumber, String date){
-        if (sum>0 && orderNumber>0 && String.valueOf(date).length()==8){
-
-        }
-    }
-
-    public int getContractsCount(){
-        return contractsCount;
-    }
-
-    public static Contracts create(){
-        return new Contracts();
-    }
-    public HashMap<String, Document> getDocs(){
-        return data;
-    }
-
-
-
-}
