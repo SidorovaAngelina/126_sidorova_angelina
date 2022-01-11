48d3999d3d73a1395ca96675ea4aec22c23308f3
SidorovaAngelina <lina_sidorova03@mail.ru>
2022-01-11 18:44:27 +0300

    task 10
    create and complete test create_CreateEmptyContBook_ContCountEqualsZero

--- task_10/src/Core/ContractsBook.java
+++ task_10/src/Core/ContractsBook.java
@@ -1,4 +1,20 @@
 package Core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ContractsBook {
+    private int contCount;
+    private ContractsBook(){
+        contCount = 0;
+    }
+    public int getContCount(){
+        return contCount;
+    }
+    public static ContractsBook create(){
+        return new ContractsBook();
+    }
+
 }
+
--- task_10/src/Tests/ContractsBookTests.java
+++ task_10/src/Tests/ContractsBookTests.java
@@ -1,4 +1,16 @@
 package Tests;
 
-public class ContractsBookTests {
+import org.junit.*;
+import Core.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ContractsBookTests extends Assert {
+    @Test
+    public void create_CreateEmptyContBook_ContCountEqualsZero() {
+        ContractsBook contractsBook = ContractsBook.create();
+        assertEquals(0, contractsBook.getContCount());
+    }
 }
