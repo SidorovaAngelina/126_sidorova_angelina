8dc58eb155fde35813239fa22c8b7ad0d48fb9e8
SidorovaAngelina <lina_sidorova03@mail.ru>
2022-03-28 21:47:47 +0300

    task_3_2sem

--- 2 sem/hw/task_3/126_sidorova_task_3/src/Task3_Tests.java
+++ 2 sem/hw/task_3/126_sidorova_task_3/src/Task3_Tests.java
@@ -8,6 +8,54 @@ public class Task3_Tests extends Assert {
             return first - second;
         }
     }
-
+    @Test
+    public void heapSort_HeapSortWithTHeSameElem_WithoutChanges(){
+        HeapSort<Integer> heapSort = new HeapSort<>();
+        Integer[] data = {2,2,2};
+        heapSort.sort(data, new TestComparator());
+        assertArrayEquals(new Integer [] {2,2,2}, data);
+    }
+    @Test
+    public void heapSort_HeapSortIsSorted_WithoutChanges(){
+        HeapSort<Integer> heapSort = new HeapSort<>();
+        Integer[] data = {2,3,4};
+        heapSort.sort(data, new TestComparator());
+        assertArrayEquals(new Integer [] {2,3,4}, data);
+    }
+    @Test
+    public void heapSort_HeapSortIsUnsorted_CorrectOrder(){
+        HeapSort<Integer> heapSort = new HeapSort<>();
+        Integer[] data = {2,4,3};
+        heapSort.sort(data, new TestComparator());
+        assertArrayEquals(new Integer [] {2,3,4}, data);
+    }
+    @Test
+    public void heapSort_HeapSortWithNegativeElem_CorrectOrder(){
+        HeapSort<Integer> heapSort = new HeapSort<>();
+        Integer[] data = {2,0,-3};
+        heapSort.sort(data, new TestComparator());
+        assertArrayEquals(new Integer [] {-3,0,2}, data);
+    }
+    @Test
+    public void heapSort_AnotherUnsorted_CorrectOrder(){
+        HeapSort<Integer> heapSort = new HeapSort<>();
+        Integer[] data = {3, 5, 1, 2, 4, 6};
+        heapSort.sort(data, new TestComparator());
+        assertArrayEquals(new Integer [] {1,2,3,4,5, 6}, data);
+    }
+    @Test
+    public void heapSort_HeapSortIsUnsortedWithSameElem_CorrectOrder(){
+        HeapSort<Integer> heapSort = new HeapSort<>();
+        Integer[] data = {7, 3, 2, 6, 1, 4, 8, 5, 9, 0, 1};
+        heapSort.sort(data, new TestComparator());
+        assertArrayEquals(new Integer[]{0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9}, data);
+    }
+    @Test
+    public void heapSort_HeapSortIsUnsortedWithSameElemAndNegativeElem_CorrectOrder(){
+        HeapSort<Integer> heapSort = new HeapSort<>();
+        Integer[] data = {7, -3, 2, 6, 1, 4, -8, 5, 9, 0, 1};
+        heapSort.sort(data, new TestComparator());
+        assertArrayEquals(new Integer[]{-8, -3, 0, 1, 1, 2, 4, 5, 6, 7, 9}, data);
+    }
 
 }
